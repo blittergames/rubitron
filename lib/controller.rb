c93f9a94ce3c6bbe4ea1de8265fc383270a54639
Haroon Khalid <haroon@blittergames.com>
2009-07-16 00:44:26 -0400

    events

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,17 +1,49 @@
+class Animation
+ 
+  attr_reader :data, :cur_frame, :ticks, :ticks_remaining, :pos
+  def initialize data
+    @my_clock = Rubygame::Clock.new()
+    @my_clock.enable_tick_events()
+    @data = data # data = [ [time, image], [time, image], ...]
+    @cur_frame = 0
+    @ticks = @my_clock.tick()
+    @ticks_remaining = data[0][0]
+    @pos = [0, 0]
+  end
+  
+  def draw dest
+    @old_ticks = @ticks
+    @ticks = @my_clock.tick()
+    @tick_difference = (@ticks - @old_ticks)
+    @ticks_remaining += tick_difference
+    @dest = dest
+  
+    while @ticks_remaining <= 0
+      @cur_frame += 1
+      @cur_frame %= @data.length
+      @ticks_remaining += @data[@cur_frame][0]
+    end
+      
+    @data[@cur_frame][1].blit(dest, @pos)
+  
+  end
+end
+
 class Player
 	include Rubygame::EventHandler::HasEventHandler
-	attr_reader :queue, :screen, :x, :y
-	attr_accessor :queue, :screen, :x, :y
+	attr_accessor :queue, :screen, :x, :y, :hurt
 	def initialize screen
 		@screen = screen
 		@queue = Rubygame::EventQueue.new()
 		@queue.enable_new_style_events()
 		@player_1_img = Surface.load("graphics/player_ship.png")
+		@player_2_img = Surface.load("graphics/player_ship_2.png")
 	    @x = 150
 		@y = 400
 		@font = TTF.new("data/Welbut__.ttf",18)
 		@text_str = "Player: " + "#{@x}" + ", " + "#{@y}"
 		@text = @font.render(@text_str,false,[214,214,214])
+		@hurt = Animation.new([[1000, @player_1_img],[1000, @player_2_img]])	
 	end
 	
 	def right
@@ -37,6 +69,8 @@ class Player
 		make_magic_hooks(movement_hooks)
 	end	
 		
+
+
 	def draw
 		@player_1_img.blit(@screen,[@x,@y])
 		@text.blit(@screen, [10,10])
@@ -79,6 +113,7 @@ class Controller
 			@p1.move()
 			fps_update()
 			@background.blit(@screen,[0,0])
+			@p1.hurt.draw(@screen)
 			@p1.draw()
 			@screen.flip()
 			@clock.tick()
